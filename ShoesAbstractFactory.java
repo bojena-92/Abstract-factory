7a3308e19a43e554a9584f0cef8022e39c248459
bojena-92 <63927528+bojena-92@users.noreply.github.com>
2020-04-18 13:09:17 -0700

    Update ShoesAbstractFactory.java

--- ShoesAbstractFactory.java
+++ ShoesAbstractFactory.java
@@ -2,5 +2,5 @@ package shoesabstractfactory;
 
 public abstract class ShoesAbstractFactory {
 	abstract Color getColor (String color);
-    abstract Brand getBrand (String brand);
+   	abstract Brand getBrand (String brand);
 }
